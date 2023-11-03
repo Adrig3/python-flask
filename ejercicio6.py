94c6965bc83ffd8349a9269c03e28c50dcba95c7
Adri <adriagarisagrera@gmail.com>
2023-11-03 15:48:43 +0100

    Ejercicio6+

--- ejercicio6.py
+++ ejercicio6.py
@@ -2,9 +2,16 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/temperatura/')
 def iva():
-    celsius = float(request.args.get('celsius', 1))
-    resultado = celsius + 32
-    cadena = f'<h2>En grados Farenheit es: {resultado}</h2>'
-    return cadena
+    celsius = float(request.args.get('celsius', -999))
+
+    if celsius == -999:
+        error = f'<h2>Se debe introducir un valor en Celsius.</h2>'
+        return error
+    else:
+
+        resultado = celsius + 32
+        cadena = f'<h2>En grados Farenheit es: {resultado}</h2>'
+        return cadena
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
